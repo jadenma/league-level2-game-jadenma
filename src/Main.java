af6bcea8389dea24820023b2ff73e99bb968b70f
Jackson-Bush <bushjackson56@gmail.com>
2023-06-12 18:30:28 -0700

    continued

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import javax.swing.JFrame;
 
 public class Main {
 
+	JFrame frame;
+	GamePanel panel;
+	public static final int WIDTH = 500;
+	public static final int LENGTH = 800;
+	
+	public Main() {
+		frame = new JFrame();
+		panel = new GamePanel();
+	}
+	
+	void setup() {
+		frame.add(panel);
+		frame.setSize(WIDTH, LENGTH);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(panel);
+	}
 	public static void main(String[] args) {
-		
+		Main game = new Main();
+		game.setup();
 	}
 }
